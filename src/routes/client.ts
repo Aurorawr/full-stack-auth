fcc1f0d468345ff1e58637ee6b1ab692934946b2
Lucas <aurorawwr@gmail.com>
2025-11-03 14:37:00 -0300

    FIX: servicio de creación de clientes

--- src/routes/client.ts
+++ src/routes/client.ts
@@ -8,12 +8,12 @@ const generateApiKey = (): string => {
 
 const router = Router();
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { name } = req.body;
   if (!name) return res.status(400).json({ message: "El nombre del cliente es obligatorio." });
-  const newClient = Client.create({name, apiKey: generateApiKey()});
+  const newClient = await Client.create({name, apiKey: generateApiKey()});
 
-  return res.status(201).json(newClient);
+  return res.status(201).json({id: newClient.id, name: newClient.name, apiKey: newClient.apiKey});
 });
 
 export default router;
