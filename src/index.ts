ce2a257e5dba62ebbad73998ae573b1d94516b73
Lucas Quintanilla <aurorawwr@gmail.com>
2025-11-03 12:58:02 -0300

    Configurando swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import express, { Request, Response } from "express";
+import cors from "cors";
+import express from "express";
 import { sequelize } from "./db";
 import clientsRouter from "./routes/client";
 import usersRouter from "./routes/user";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger-output.json"
 
 const app = express();
 const PORT = 3000;
@@ -24,10 +27,12 @@ start();
 
 // Middleware para JSON
 app.use(express.json());
+app.use(cors());
 
 // Ruta de prueba
 app.use("/clients", clientsRouter);
 app.use("/users", usersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // Iniciar servidor
 app.listen(PORT, () => {
