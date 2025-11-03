6bb53cc15f8edb315e9d7c34ca74bcb2c88e371a
Lucas Quintanilla <aurorawwr@gmail.com>
2025-11-03 12:11:50 -0300

    Configurando sequelize y creando rutas de clients

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import express, { Request, Response } from "express";
+import { sequelize } from "./db";
+import clientsRouter from "./routes/client";
 
 const app = express();
 const PORT = 3000;
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión a PostgreSQL exitosa");
+
+    // Sincroniza modelos con la base de datos
+    await sequelize.sync({ alter: true }); // crea tablas si no existen
+    console.log("Modelos sincronizados");
+
+  } catch (error) {
+    console.error("Error de conexión:", error);
+  }
+};
+
+start();
 
 // Middleware para JSON
 app.use(express.json());
 
 // Ruta de prueba
-app.get("/", (req: Request, res: Response) => {
-  res.send("¡Hola desde Express + TypeScript!");
-});
+app.use("/clients", clientsRouter);
 
 // Iniciar servidor
 app.listen(PORT, () => {
